8ce2a0c448c16b093ccd4dc9aa529bfdd41065fc
Paula Ramon <paularamon@Paulas-MacBook-Pro.local>
2019-05-30 18:38:17 +0100

    initial commit

---
+++ project3.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+   return render_template("helloworld.html")
+
+@app.route("/<name>")
+def hi(name):
+   return render_template("helloworld.html", userName=name)
