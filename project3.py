60855569fa8f10bf6f640cd779b2fc91c48129b5
Paula Ramon <paularamon@Paulas-MacBook-Pro.local>
2019-05-30 19:13:15 +0100

    asd

--- project3.py
+++ project3.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+#app = Flask("MyApp")
+@app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/tda", methods=["POST"])
+def tda():
+    form_data = request.form
+    #tda = form_data["tda"]
+    #print (tda)
+    return render_template("tda.html")
 
-@app.route('/')
-def hello():
-   return render_template("project3.html")
+@app.route("/signup", methods=["POST"])
+def signup():
+    form_data = request.form
+    return render_template("signup.html")
+    #send_message(form_data["email"])
 
-@app.route("/<name>")
-def hi(name):
-   return render_template("project3.html", userName=name)
+
+#def send_message(email):
+	#print(email)
+	#return requests.post("https://api.mailgun.net/v3/sandboxbec895c8e12a4174a2462f053a6bad7c.mailgun.org/messages",
+        #auth=("api", "41f4e00ee0156073d275f5162b7ea638-e566273b-8f7e8104"),
+        #data={"from": "The Day After <mailgun@sandboxbec895c8e12a4174a2462f053a6bad7c.mailgun.org>",
+             ## "to": [email],
+             # "subject": "Hello",
+             # "text": "Thank you for subscribing!"})
---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+/* 10 carousel example */
+ $('#carousel-10').carousel({
+     interval: 2000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 11 carousel example */
+ $('#carousel-11').carousel({
+     interval: 4000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 12 carousel example */
+ $('#carousel-12').carousel({
+     interval: 6000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 13 carousel example */
+ $('#carousel-13').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
